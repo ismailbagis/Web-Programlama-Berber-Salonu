8d32ae2eb408c36153c000c47f557ab36ed0c192
tunahanozdemirr <134001816+tunahanozdemirr@users.noreply.github.com>
2024-12-12 17:55:21 +0300

    deneme

---
+++ Barber-website/Models/Doctor.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eci_Barber.Models
+{
+    public class Doctor
+    {
+        public int MyProperty { get; set; }
+    }
+}
